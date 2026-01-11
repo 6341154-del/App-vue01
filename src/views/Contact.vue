b1960dc7c3e0a8b6b879109eb622b1d59dceb4f5
6341154-del <6341154@techno-chon.ac.th>
2026-01-11 16:50:18 +0700

    update v1

---
+++ src/views/Contact.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h1>Contact to Jaruda Thaipom</h1>
+        <h3>Tal.064-229-5944</h3>
+        <img alt="Vue logo" src="../assets/audii.jpg">
+    </div>
+</template>
+
