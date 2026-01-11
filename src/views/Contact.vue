f5fa7113865f1b3b3f30bed8536c4e935cbee9a0
Suthidarrr <suthida12679@gmail.com>
2026-01-11 16:43:02 +0700

    update v1

---
+++ src/views/Contact.vue
@@ -0,0 +1,7 @@
+<template lang="">
+    <div>
+        <h1>contact to suthida ruangsuksud</h1>
+        <h3>Tel.090-082-6952</h3>
+        <img alt="Vue logo" src="../assets/cat.jpg">
+    </div>
+</template>
